3565989d1235d058fd2ad53a3487315debda5b71
Fred120-code <fredayemtsa@gmail.com>
2025-07-27 19:56:35 +0100

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
+import {IoMdSearch} from 'react-icons/io'
+
+const MenuLinks = [
+    {
+        id:1,
+        name:"Home",
+        link:"/#",
+    },
+    {
+        id:2,
+        name:"Shop",
+        link:"/#shop",
+    },
+    {
+        id:3,
+        name:"Blog",
+        link:"/#blog",
+    },
+    {
+        id:4,
+        name:"About",
+        link:"/#about",
+    },
+]
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
+        <div className='py-4'>
             <div className="container">
-                <div>
+                {/**Logo and Links section */}
+                <div className="flex items-center gap-4">
                     <a href="#"
-                    className='text-primary'
+                    className='text-primary font-semibold tracking-widest text-2xl
+                     uppercase sm:text-3xl'
                     >Eshop</a>
+                    {/**Menu Items */}
+                    <div className="hidden lg:block">
+                        <ul className="flex items-center gap-4 ">
+                            {MenuLinks.map((data, index) => (
+                                <li key={index}>
+                                    <a href={data.link}
+                                    className=" inline-block px-4 font-semibold text-gray-500
+                                     hover:text-black dark:hover:text-white duration-200"
+                                    > {data.name}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                {/**navbar Right section */}
+                <div>
+                    {/**Search Bar section */}
+                    <div>
+                        <input type="text" placeholder="Search"
+                        className="" />
+                        <IoMdSearch/>
+                    </div>
+
+
+                    {/**Dark mode section */}
                 </div>
             </div>
         </div>
