490c5f95089fb742bb77079c7b105cc512274395
Fred120-code <fredayemtsa@gmail.com>
2025-07-27 17:20:18 +0100

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div className="container">
+                <div>
+                    <a href="#"
+                    className='text-primary'
+                    >Eshop</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
