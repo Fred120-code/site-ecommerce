60e11d0874f503899436699a7ceccbef13273bfe
Fred120-code <fredayemtsa@gmail.com>
2025-07-28 15:55:37 +0100

    ajout du dark mode

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
+import { FaCartShopping } from 'react-icons/fa6'
 import {IoMdSearch} from 'react-icons/io'
+import DarkMode from '../Navbar/DarkMode'
 
 const MenuLinks = [
     {
@@ -27,7 +29,7 @@ const Navbar = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
         <div className='py-4'>
-            <div className="container">
+            <div className="container flex justify-between items-center">
                 {/**Logo and Links section */}
                 <div className="flex items-center gap-4">
                     <a href="#"
@@ -49,16 +51,27 @@ const Navbar = () => {
                     </div>
                 </div>
                 {/**navbar Right section */}
-                <div>
+                <div className='flex justify-between items-center gap-4'>
                     {/**Search Bar section */}
-                    <div>
+                    <div className='relative group hidden sm:block'>
                         <input type="text" placeholder="Search"
-                        className="" />
-                        <IoMdSearch/>
+                        className="search-bar" />
+                        <IoMdSearch className='text-xl text-gray-600 dark:text-gray-400
+                         absolute top-1/4 right-3 group-hover:text-primary duration-200'/>
                     </div>
 
-
+                    {/**order button section*/}
+                    <button className='relative p-3'>
+                        <FaCartShopping className='text-gray-600 dark:text-gray-400 text-xl'/>
+                        <div className='w-4 h-4 bg-red-500 text-white rounded-full absolute top-0
+                            right-0 flex items-center justify-center text-xs'>
+                            4
+                        </div>
+                    </button>
                     {/**Dark mode section */}
+                    <div>
+                        <DarkMode/>
+                    </div>
                 </div>
             </div>
         </div>
