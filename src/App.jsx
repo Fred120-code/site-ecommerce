8decf8b687187e044abfc7c3ed369ca130d75bf6
Fred120-code <fredayemtsa@gmail.com>
2025-08-02 17:19:37 +0100

    fin de la section Banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,20 @@ import Hero from './components/Hero/Hero'
 import Category from './components/Category/Category'
 import Category2 from './components/Category/Category2'
 import Services from './components/Services/Services'
+import Banner from './components/Banner/Banner'
+import casque_2 from '../src/assets/casuqe_2.png'
+
+const BannerData = {
+  discount:"30% OFF", 
+  title:"Fine Smile",
+  date:"10 Jan to 28 jan",
+  image: casque_2,
+  title2:"Air Solo Bass",
+  title3:"Winter Sale",
+  title4:"Lorem ipsun sit amet consectetur adipisicing alit. eaque reiciendis",
+  bgColor:"#f42c37",
+}
+
 const App = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
@@ -11,6 +25,7 @@ const App = () => {
       <Category/>
       <Category2/>
       <Services/>
+      <Banner data = {BannerData}/>
     </div>
   )
 }
