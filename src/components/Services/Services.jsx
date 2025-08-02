240db00c02cd0d93463fae0eba847386266faa6d
Fred120-code <fredayemtsa@gmail.com>
2025-08-02 11:57:41 +0100

    fin de la sections service

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,8 +1,12 @@
-import { FaCarSide, FaHeadphoneAlt, FaWallet, FaCheckCircle, } from 'react-icons/fa'
+import React from 'react';
+import {
+        FaCarSide, 
+        FaHeadphonesAlt, 
+        FaWallet, 
+        FaCheckCircle, 
+    }  from 'react-icons/fa'
 
-const Services = () => {
-
-  const ServiceData = [
+const ServiceData = [
     {
         id:1,
         icon:<FaCarSide className='text-4xl md:text-5xl text-primary'/>,
@@ -23,31 +27,29 @@ const Services = () => {
     },
     {
         id:4,
-        icon:<FaHeadphoneAlt className='text-4xl md:text-5xl text-primary'/>,
+        icon:<FaHeadphonesAlt className='text-4xl md:text-5xl text-primary'/>,
         title:"Online Support 24/7",
         description:"Technical Support 24/7",
     },
 ];
 
-  return (
-    <div>
-        <div className='container'>
+const Services = () => {
+
+  return <div>
+        <div className='container my-14 md:my-20'>
             <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8'>
-                {
-                    ServiceData.map((data)=>(
-                        <div key={data.id}>
+                {ServiceData.map((data) => (
+                        <div key={data.id} className='flex flex-col items-center sm:flex-row gap-4'>
                             {data.icon}
                             <div>
-                                <h2>{data.title}</h2>
-                                <p>{data.description}</p>
+                                <h1 className='lg:text-xl font-bold '>{data.title}</h1>
+                                <p className='text-gray-400 text-sm'>{data.description}</p>
                             </div>
                         </div>
-                    ))
-                }
+                ))}
             </div>
         </div>
     </div>
-  )
 }
  
 export default Services
