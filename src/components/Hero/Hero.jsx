903f1af4f86772543be4435ca22565209496860c
Fred120-code <fredayemtsa@gmail.com>
2025-08-17 02:06:36 +0100

    animation avec aos

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,89 +1,122 @@
-import React from 'react'
-import Slider from 'react-slick';
-import doubleWatch from '../../assets/doubleWatch.png'
-import casque_9 from '../../assets/casque_9.png'
-import casque_1 from '../../assets/casque_1.png'
-import Button from '../Shared/Button';
+import React from "react";
+import Slider from "react-slick";
+import doubleWatch from "../../assets/doubleWatch.png";
+import casque_9 from "../../assets/casque_9.png";
+import casque_1 from "../../assets/casque_1.png";
+import Button from "../Shared/Button";
 
-const Hero = ({handleOrderPopup}) => {
-   const settings = {
+const Hero = ({ handleOrderPopup }) => {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplaySpeed:1,
-    cssEase:"ease-in-out",
+    autoplaySpeed: 1,
+    cssEase: "ease-in-out",
     pauseOnFocus: true,
   };
 
   const HeroData = [
     {
-      id:1,
+      id: 1,
       img: casque_1,
       subtitle: "Beats Solo",
       title: "Wireless",
-      title2:"Headphone"
-   },
-     {
-      id:2,
+      title2: "Headphone",
+    },
+    {
+      id: 2,
       img: doubleWatch,
       subtitle: "Beats Solo",
       title: "Wireless",
-      title2:"Headphone",
-     },
-     {
-      id:3,
+      title2: "Headphone",
+    },
+    {
+      id: 3,
       img: casque_9,
       subtitle: "Beats Solo",
       title: "Wireless",
-      title2:"Headphone",
-     },
-  ]
+      title2: "Headphone",
+    },
+  ];
 
   return (
-    <div className=' container'>
-        <div className=' overflow-hidden rounded-3xl min-h-[550px]
-         sm:min-h-[650px] hero-bg-color flex justify-center items-center'>
-          {/**Hero section */}
-          <div className='container pb-8 sm:pb-0 '>
+    <div className=" container">
+      <div
+        className=" overflow-hidden rounded-3xl min-h-[550px]
+         sm:min-h-[650px] hero-bg-color flex justify-center items-center"
+      >
+        {/**Hero section */}
+        <div className="container pb-8 sm:pb-0 ">
           <Slider {...settings}>
-              {
-                HeroData.map((data)=>(
-                  <div key={data.id}>
-                    <div className='grid grid-cols-1 sm:grid-cols-2'>
-                      {/**text content section */}
-                      <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center
-                       sm:text-left order-2 sm:order-1 relative z-10'>
-                        <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
-                        <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
-                        <h1 className='text-5xl uppercase text-white dark:text-white/5 sm:text-[80px]
-                         md:text-[100px] font-bold xl:text-[150px]'>{data.title2}</h1>
-                        <div className=''>
-                          <Button text="Shop By Category"
-                          bgColor="bg-primary"
-                          textColor="text-white"
-                          handler={handleOrderPopup}
-                          />
-                        </div>
-                      </div>
-                      {/**Img section */}
-                      <div className='order-1 sm:order-2'>
-                        <div>
-                          <img src={data.img} alt="" className='w-[250px] h-[300px]
-                             sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-110 object-contain
-                             mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40'/>
-                        </div>
-                      </div>
+            {HeroData.map((data) => (
+              <div key={data.id}>
+                <div className="grid grid-cols-1 sm:grid-cols-2">
+                  {/**text content section */}
+                  <div
+                    className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center
+                       sm:text-left order-2 sm:order-1 relative z-10"
+                  >
+                    <h1
+                      data-aos="zoom-out"
+                      data-aos-duration="500"
+                      data-aos-once="true"
+                      className="text-2xl sm:text-6xl lg:text-2xl font-bold"
+                    >
+                      {data.subtitle}
+                    </h1>
+                    <h1
+                      data-aos="zoom-out"
+                      data-aos-duration="500"
+                      data-aos-once="true"
+                      className="text-5xl sm:text-6xl lg:text-7xl font-bold"
+                    >
+                      {data.title}
+                    </h1>
+                    <h1
+                      data-aos="zoom-out"
+                      data-aos-duration="500"
+                      data-aos-once="true"
+                      className="text-5xl uppercase text-white dark:text-white/5 sm:text-[80px]
+                         md:text-[100px] font-bold xl:text-[150px]"
+                    >
+                      {data.title2}
+                    </h1>
+                    <div
+                      data-aos="fade-up"
+                      data-aos-offset="0"
+                      data-aos-duration="500"
+                      data-aos-delay="300"
+                    >
+                      <Button
+                        text="Shop By Category"
+                        bgColor="bg-primary"
+                        textColor="text-white"
+                        handler={handleOrderPopup}
+                      />
                     </div>
                   </div>
-                ))
-              }
+                  {/**Img section */}
+                  <div className="order-1 sm:order-2">
+                    <div>
+                      <img
+                        src={data.img}
+                        alt=""
+                        className="w-[250px] h-[300px]
+                             sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-110 object-contain
+                             mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </Slider>
-          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
